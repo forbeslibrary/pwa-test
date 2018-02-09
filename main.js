e8537034f6b134bc28894e35a99fdf04716e4a07
bkalish <bkalish@gmail.com>
2018-02-09 16:56:39 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var wp_server = "https://forbeslibrary.org/";
+//var wp_server ="http://localhost:8080/wordpress/";
+$(document).ready(function() {
+  $.ajax({
+    url: wp_server + "wp-json/wp/v2/posts",
+    data: {
+      "categories": ["300"]
+    }
+  }).then(function(data) {
+    $.each(data, function (index, value) {
+      $("#news-placeholder").remove();
+      $("#news").append("<p>" + value.title.rendered + "</p>");
+    });
+    console.log(data);
+  });
+  
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then(function() { console.log('Service Worker Registered'); });
+  }
+});
