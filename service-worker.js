ce3ba1d4c6c5412dab7c1e78147a14ba01238767
bkalish <bkalish@gmail.com>
2018-02-20 09:12:10 -0500

    adding homeURL variable to service worker

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
 var cacheName = 'forbeslibrary-v1';
+var homeURL = "https://forbeslibrary.github.io/pwa-test/";
 var staticFilesForAppShell = [
-  '',
-  'manifest.json',
-  'style.less',
-  'main.js',
-  'img/coolidge-portrait-small.jpg',
-  'img/digital-gallery-button.png',
+  homeURL,
+  homeURL + 'manifest.json',
+  homeURL + 'style.less',
+  homeURL + 'main.js',
+  homeURL + 'img/coolidge-portrait-small.jpg',
+  homeURL + 'img/digital-gallery-button.png',
   'https://fonts.googleapis.com/css?family=Lato',
   'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/less.js/3.0.0/less.min.js',
